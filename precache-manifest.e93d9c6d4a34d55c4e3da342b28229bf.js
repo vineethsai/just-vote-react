fa575f539cda114045b87ee16ae3d0b00e79ef97
Vineeth Sai <abcvineeth.sai@gmail.com>
2018-11-19 01:38:46 -0800

    Updates

---
+++ precache-manifest.e93d9c6d4a34d55c4e3da342b28229bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbbbd4f9930af12b3229",
+    "url": "/just-vote-react/static/js/runtime~main.cbbbd4f9.js"
+  },
+  {
+    "revision": "7711d4ca36a57f34d9a0",
+    "url": "/just-vote-react/static/js/main.7711d4ca.chunk.js"
+  },
+  {
+    "revision": "2f55af1a88bf090c1c1d",
+    "url": "/just-vote-react/static/js/1.2f55af1a.chunk.js"
+  },
+  {
+    "revision": "7711d4ca36a57f34d9a0",
+    "url": "/just-vote-react/static/css/main.31f72588.chunk.css"
+  },
+  {
+    "revision": "2f55af1a88bf090c1c1d",
+    "url": "/just-vote-react/static/css/1.8c5ac234.chunk.css"
+  },
+  {
+    "revision": "c8f098c4aa614593cd4eab9f2f6872e9",
+    "url": "/just-vote-react/index.html"
+  }
+];
