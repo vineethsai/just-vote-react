216f36626057dc52b17a5c198c849a9bb2ae42b0
Vineeth Sai <abcvineeth.sai@gmail.com>
2018-11-19 01:18:23 -0800

    need to center coloumn

---
+++ src/Components/Projects.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ProjectItem from './ProjectItems';
+
+class Projects extends Component {
+  deleteProject(id){
+    this.props.onDelete(id);
+  }
+
+  render() {
+    let projectItems;
+    if(this.props.projects){
+      projectItems = this.props.projects.map(project => {
+        //console.log(project);
+        return (
+          <ProjectItem onDelete={this.deleteProject.bind(this)} key={project.title} project={project} />
+        );
+      });
+    }
+    return (
+      <div className="Projects">
+        <h3>Latest Projects</h3>
+        {projectItems}
+      </div>
+    );
+  }
+}
+
+
+export default Projects;
