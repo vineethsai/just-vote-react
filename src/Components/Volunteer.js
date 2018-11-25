d45fccbd5d56be9124f6d837a902153d2a52f682
Madison Laughlin <laughmj5@uw.edu>
2018-11-24 22:16:03 -0800

    Fixed errors and added comments

--- src/Components/Volunteer.js
+++ src/Components/Volunteer.js
@@ -1,42 +1,46 @@
 import React, { Component } from 'react';
 import { Container, Button, Form, Input, Row, Col } from 'reactstrap';
-//import Result from './Components/Result';
 import * as d3 from "d3";
 
+// Renders search bar & list of volunteer opportunities.
 class Volunteer extends Component {
     constructor() {
         super();
         this.state = {
-            allOps: '',
-            results: '',
+            allOps: [],
+            results: [],
             searchTerm: '',
             displayOrder: true
         };
     }
 
+    // Loads data into application
     componentDidMount() {
         d3.csv('https://data.bloomington.in.gov/datastore/dump/d4aa517d-3d2a-46f6-93b5-6d43345e9bf5?bom=True')
             .then((op) => {
                 this.setState(
-                    {results: op,
-                    allOps: op}
-                    );
+                    {
+                        results: op,
+                        allOps: op
+                    }
+                );
             })
             .catch((err) => {
                 console.error(err);
             })
     }
 
+    // Stores search term into state as it's being typed into the input bar
     handleChange = (event) => {
         event.preventDefault();
         let value = event.target.value;
-        this.setState({searchTerm: value});
+        this.setState({ searchTerm: value });
     }
 
+    // Filters through data based on search term upon clicking search button
     handleSearch = (event) => {
         event.preventDefault();
-        this.loadData();
-        let searchResults = this.state.results.filter((opportunity) => {
+        let searchResults = this.state.allOps.filter((opportunity) => {
             if (opportunity.AgencyName.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) >= 0) {
                 return true;
             } else if (opportunity.WhatWeDo.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) >= 0) {
@@ -45,9 +49,10 @@ class Volunteer extends Component {
                 return false;
             }
         });
-        this.setState({results: searchResults});
+        this.setState({ results: searchResults });
     }
 
+    // Changes the order of the search results
     changeOrder = () => {
         if (this.state.displayOrder) {
             this.state.results.sort((a, b) => {
@@ -64,52 +69,66 @@ class Volunteer extends Component {
         }
     }
 
+    // Switches sort order to ascending
     handleAscending = (event) => {
         event.preventDefault();
-        this.setState({displayOrder: true});
+        this.setState({ displayOrder: true });
+        this.changeOrder();
     }
 
+    // Switches sort order to descending
     handleDescending = (event) => {
         event.preventDefault();
-        this.setState({displayOrder: false});
+        this.setState({ displayOrder: false });
+        this.changeOrder();
     }
 
+    // Shows all opportunities again.
     handleAll = (event) => {
         event.preventDefault();
-        this.loadData();
+        this.setState({ results: this.state.allOps });
     }
 
     render() {
-        console.log(this.state.results);
-        return(
-            <div className='text-center'>
-                <h1>Just Volunteer</h1>
-                <br />
-                <br />
-                <h2>Enter keywords to find volunteer opportunities that might interest you:</h2>
-                <Container fluid>
-                    <Row>
-                        <Col sm="12" md={{ size: 6, offset: 3 }}>
-                            <Form>
-                                <Input aria-label='Search' type='text' onChange={this.handleChange} />
-                                <Button  addonType="append" type="submit" className="btn btn-info"  onClick={this.handleSearch}>Search</Button>
-                                <Button type="sort" className="btn btn-info" onClick={this.handleAscending}>Sort Ascending</Button>
-                                <Button type="sort" className="btn btn-info" onClick={this.handleDescending}>Sort Descending</Button>
-                                <Button className="btn btn-info" onClick={this.handleAll}>Show All</Button>
-                            </Form>
-                        </Col>
-                    </Row>
-                </Container>
-                <br />
-                {this.state.results.forEach((opportunity) => {return <Result opportunity={opportunity} />})}
+        return (
+            <div>
+                <div className='text-center'>
+                    <h1>Just Volunteer</h1>
+                    <br />
+                    <br />
+                    <h2>Enter keywords to find volunteer opportunities that might interest you:</h2>
+                    <Container fluid>
+                        <Row>
+                            <Col sm="12" md={{ size: 6, offset: 3 }}>
+                                <Form>
+                                    <Input aria-label='Search' type='text' onChange={this.handleChange} />
+                                    <Button addonType="append" type="submit" className="btn btn-info v-btn" onClick={this.handleSearch}>Search</Button>
+                                    <Button type="sort" className="btn btn-info v-btn" onClick={this.handleAscending}>Sort Ascending</Button>
+                                    <Button type="sort" className="btn btn-info v-btn" onClick={this.handleDescending}>Sort Descending</Button>
+                                    <Button className="btn btn-info v-btn" onClick={this.handleAll}>Show All</Button>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <br />
+                    {this.state.results.map((opportunity) => { return <Result opportunity={opportunity} /> })}
+                </div>
+                <footer>
+                    <address className="text-center">&copy; Madison Laughlin 2018 <br />
+                        <a href="mailto:laughmj5@uw.edu">Email here</a><br />
+                        or <a href="tel:+1-206-819-5148">call this number.</a><br />
+                        Data from <a href="https://catalog.data.gov/dataset/bloomington-volunteer-network-partner-organizations">City of Bloomington Volunteer Network Website</a>
+                    </address>
+                </footer>
             </div>
         )
     }
 }
 
+// Renders a singular volunteer opportunity.
 class Result extends Component {
     render() {
-        return(
+        return (
             <div>
                 <h3>{this.props.opportunity.AgencyName}</h3>
                 <p>{this.props.opportunity.WhatWeDo}</p>
@@ -120,5 +139,6 @@ class Result extends Component {
         )
     }
 }
+
 
 export default Volunteer;
